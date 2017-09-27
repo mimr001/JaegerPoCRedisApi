ad6de70eab7b7bfb729a35d95c54fd41c5276e9c
Michael Mrazek <michael.mrazek@gmail.com>
2017-09-27 12:53:14 +0200

    Working but not running in Docker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 ////////////////////// START Jaeger Stuff /////////////////////////
-// Auto instrumentation MUST BE ON FIRST LINE!!!
+// Auto instrumentation MUST BE ON FIRST LINE TO KICK IN!!!
 const Instrument = require('@risingstack/opentracing-auto')
 
 // Jaeger tracer (standard distributed tracing)
@@ -12,8 +12,8 @@ const sampler = new jaeger.RateLimitingSampler(1)
 // Docker stack in this PoC. Real case scenario, the Jaeger server parts will either run in the same
 // Docker stack or in a separate Docker stack but on the same host to avoid network latency to the reporter
 const reporter = new jaeger.RemoteReporter(new UDPSender({
-  host: 'docker.for.mac.localhost',
-  // host: 'localhost',
+  // host: 'docker.for.mac.localhost',
+  host: 'localhost',
   port: 6832
 }))
 const jaegerTracer = new jaeger.Tracer('jaeger-poc-redisapi-jaeger-tracer', reporter, sampler)
@@ -24,57 +24,40 @@ const MetricsTracer = require('@risingstack/opentracing-metrics-tracer')
 const prometheusReporter = new MetricsTracer.PrometheusReporter()
 const metricsTracer = new MetricsTracer('jaeger-poc-redisapi-metrics-tracer', [prometheusReporter])
 
+const tracers = [metricsTracer, jaegerTracer]
 const instrument = new Instrument({
-  tracers: [metricsTracer, jaegerTracer]
+  tracers: tracers
 })
+
+// Extra cfg needed for redis instrumentation
+const instrumentation = require('@risingstack/opentracing-auto/src/instrumentation/redis')
 ////////////////////// END Jaeger Stuff /////////////////////////
 
+// THESE GET AUTO INSTRUMENTED THANKS TO THE FIRST LINE
 const express = require('express')
 const http = require('http')
 const redis = require('redis')
+instrumentation.patch(redis, tracers)
 
 var app = express()
-var redisClient = redis.createClient(6379, 'redis')
+// var redisClient = redis.createClient(6379, 'redis')
+var redisClient = redis.createClient(6379, 'localhost')
 
 app.get('/counter', function(req, res, next) {
-  // // TODO Fix this with .map() call on instrument.tracers instead
-  var metricsSpan = createRpcSpan('GET/', req, metricsTracer)
-  var jaegerSpan = createRpcSpan('GET/', req, jaegerTracer)
-  var spans = [metricsSpan, jaegerSpan]
-
-  console.log('In Redis API endpoint, calling redis')
-  jaegerSpan.log({info: 'In Redis API endpoint, calling redis'})
-
   redisClient.incr('counter', function(err, counter) {
     if(err) {
-      jaegerSpan.log({error: 'Error calling redis' + e})
-      spans.map((s) => s.setTag(Tags.HTTP_STATUS_CODE, 500)) // Indicate error
-      spans.map((s) => s.finish()) // Close spans
       return next(err)
     }
 
-    spans.map((s) => s.finish()) // Close spans
     res.send('Redis counter is: ' + counter)
   })
 })
 
-function createRpcSpan(name, req, tracer) {
-  // Instrumentation, check for any relevant http headers (debug ids etc)
-  const span = tracer.startSpan(name, {
-    childOf: tracer.extract(FORMAT_HTTP_HEADERS, req.headers)
-  })
-  const headers = {}
-
-  tracer.inject(span, FORMAT_HTTP_HEADERS, headers)
-
-  span.setTag(Tags.HTTP_URL, req.url)
-  span.setTag(Tags.HTTP_METHOD, req.method || 'GET')
-  // FIXME How do we know that here??? Should prob be set after success/failed call
-  span.setTag(Tags.HTTP_STATUS_CODE, 200)
-  span.setTag(Tags.SPAN_KIND_RPC_CLIENT, true)
-
-  return span
-}
+// Metrics endpoint, typically for scraping with Prometheus or equivalent
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', MetricsTracer.PrometheusReporter.Prometheus.register.contentType)
+  res.end(prometheusReporter.metrics())
+})
 
 http.createServer(app).listen(8081, function() {
   console.log('Listening on port 8081')
